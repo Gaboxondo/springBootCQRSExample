9979c1b340fd651e341efd1550db4a35d3643cc4
MSI\gabig <gabigarciagar@gmail.com>
2024-04-24 15:56:57 +0200

    init of the project with all docker compose services and the two involved microservices

---
+++ AccountQuery/src/test/java/com/mashosoft/AccountQuery/AccountQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashosoft.AccountQuery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
