73946a08a7003fc7a3aeff6ad9c1a5a717104a10
MSI\gabig <gabigarciagar@gmail.com>
2024-04-25 13:06:56 +0200

    aggregate account finished and also fixed a lombock error

--- AccountCommand/src/main/java/com/mashosoft/AccountCommand/domain/aggregates/AccountAggregate.java
+++ AccountCommand/src/main/java/com/mashosoft/AccountCommand/domain/aggregates/AccountAggregate.java
@@ -1,7 +1,9 @@
 package com.mashosoft.AccountCommand.domain.aggregates;
 
 import com.mashosoft.AccountCommand.domain.events.AccountOpenedEvent;
+import com.mashosoft.AccountCommand.domain.events.CloseAccountEvent;
 import com.mashosoft.AccountCommand.domain.events.DepositMoneyEvent;
+import com.mashosoft.AccountCommand.domain.events.WithdrawMoneyEvent;
 import com.mashosoft.AccountCommand.eventFrameworkCore.commands.AggregateRoot;
 import com.mashosoft.AccountCommand.interfaces.web.commandsDto.OpenAccountCommandDTO;
 
@@ -42,5 +44,37 @@ public class AccountAggregate extends AggregateRoot {
     public void apply(DepositMoneyEvent event){
         this.id = event.getId();
         this.balance += event.getAmount();
+    }
+
+    public void withdrawMoney(Double amount){
+        if(!this.active){
+            throw new IllegalStateException("Not active account");
+        }
+        if(amount<= 0){
+            throw new IllegalStateException("Amount must be greater than 0");
+        }
+        raiseEvent( WithdrawMoneyEvent.builder()
+            .id(this.id)
+            .amount( amount )
+            .build());
+    }
+
+    public void apply(WithdrawMoneyEvent event){
+        this.id = event.getId();
+        this.balance -= event.getAmount();
+    }
+
+    public void closeAccount(){
+        if(!this.active){
+            throw new IllegalStateException("The account is already closed");
+        }
+        raiseEvent( CloseAccountEvent.builder()
+            .id(this.id)
+            .build());
+    }
+
+    public void apply(CloseAccountEvent event){
+        this.id = event.getId();
+        this.active = false;
     }
 }
--- AccountCommand/src/main/java/com/mashosoft/AccountCommand/domain/events/CloseAccountEvent.java
+++ AccountCommand/src/main/java/com/mashosoft/AccountCommand/domain/events/CloseAccountEvent.java
@@ -8,7 +8,6 @@ import lombok.experimental.SuperBuilder;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @SuperBuilder
 public class CloseAccountEvent extends BaseEvent {
 }
