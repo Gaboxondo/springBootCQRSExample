26bf4d4fdc85746b1799558a5ad14b3fd847aec3
MSI\gabig <gabigarciagar@gmail.com>
2024-04-29 15:17:39 +0200

    improving pgadmin connection. also trying to use the ddl to autocreate the tables

--- AccountQuery/src/main/java/com/mashosoft/AccountQuery/infrastructure/postgressdb/entity/AccountEntityJpa.java
+++ AccountQuery/src/main/java/com/mashosoft/AccountQuery/infrastructure/postgressdb/entity/AccountEntityJpa.java
@@ -3,6 +3,7 @@ package com.mashosoft.AccountQuery.infrastructure.postgressdb.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "accounts")
 public class AccountEntityJpa {
 
     @Id
